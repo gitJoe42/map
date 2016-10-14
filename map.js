176d07fbb61c29e5e10dffc39a46fdc64e01c5c5
gitJoe42 <email@jochenkunze.de>
2016-10-14 16:34:40 +0200

    map Tutor abgeschlossen

--- map.js
+++ map.js
@@ -1,3 +1,7 @@
 var numbers = [1, 4, 9];
 var roots = numbers.map(Math.sqrt);
 // roots ist [1, 2, 3], numbers ist immernoch [1, 4, 9]
+console.log("Numbers:");
+numbers.forEach(function (element, index, array) {console.log(element)});
+console.log("Roots:");
+roots.forEach(function (element, index, array) {console.log(element)});
